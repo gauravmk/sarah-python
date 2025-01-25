401b6b18a65518022e703443250f43fde4cbbb5c
Sarah Bassiouni <sbassiou@umich.edu>
2025-01-24 20:24:51 -0500

    Updated code with in-person tutoring.

--- 3-conditionals.py
+++ 3-conditionals.py
@@ -9,8 +9,8 @@ else:
     print("Number is greater than 10")
 
 # Task: A user must be 21 or older to access a website.
-birth_year = input("What year were you born? ")
-if (): # TODO: fill me in
+birth_year = 2005
+if (birth_year <=2004): # TODO: fill me in
     print("Welcome in!")
 else:
     print("You are too young to enter.")
@@ -23,18 +23,20 @@ else:
 5 >= 3 # Greater than or equal to
 5 <= 3 # Less than or equal to
 
+# Things you can check with strings; each could be wrapped in an "IF" block
 "apple" == "apple" # Equal to
 "apple".startswith("app") # Starts with
 "l" in "apple" # Contains
 
+#an example with an list: does this list contain an element?
 "apple" in ["apple", "banana", "cherry"] # Contains
 
 # Movie Tasks!
 
 movie_database = {
     "Titanic": {
-        "director": "James Cameron",
-        "actors": ["Leonardo DiCaprio", "Kate Winslet"],
+        "director": "James Cameron", #string
+        "actors": ["Leonardo DiCaprio", "Kate Winslet"], #list with square brackets
         "year": 1997
     },
     "Memento": {
@@ -59,12 +61,29 @@ movie_database = {
     }
 }
 
-movieName = "Titanic" # For the tasks, make sure to change this to test different movies
+#movieName = "The Prestige" # For the tasks, make sure to change this to test different movies
+#movieName = "Memento"
+#movieName = "Titanic"
+#movieName = "Inception"
+movieName = "Interstellar"
+
 movie = movie_database[movieName]
 
+#print(movie["actors"])
+
 # Task 1: I only like movies directed by Christopher Nolan from before 2010. 
-# Add a conditional that says "I like this movie! It's got my favorite director" if the movie meets the criteria.
+# So create a conditional that says "I like this movie! It's got my favorite director" if the movie meets the criteria.
+
+if (movie["director"]=="Christopher Nolan") and (movie["year"]<2010):
+    print("I like this movie! It's got my favorite director!")
+else:
+    print("No director matches")
 
 
 # Task 2: Actually, I like any movie that has either Leonardo DiCaprio or Christian Bale in it.
 # Add a conditional that says "I like this movie! It's got my favorite actor" if the movie meets the criteria.
+
+if "Leonardo DiCaprio" in movie["actors"] or "Christian Bale" in movie["actors"]:
+    print("I like this movie! It's got my favorite actor!")
+else:
+    print("No actor matches")
