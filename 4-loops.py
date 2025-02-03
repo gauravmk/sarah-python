3d3986b96c5f466e7a2f04636f785acc13bc7612
Sarah Bassiouni <sbassiou@umich.edu>
2025-02-02 20:19:36 -0500

    Worked through exercises and made notes.

--- 4-loops.py
+++ 4-loops.py
@@ -7,10 +7,10 @@ while number < 10:
     number = number + 1
 print("Done with the while loop")
 
-# Here's another example handling a login
-logged_in = False
+# Here's asnother example handling a login
+logged_in = True
 while not logged_in:
-    password = input("What's the password? ")
+    #password = input("What's the password? ")
     if password == "open sesame":
         logged_in = True
     else:
@@ -28,15 +28,40 @@ initial_number = 3
 # TODO: Implement the steps above printing out each number you get until you reach 1.
 # For example, for the initial number 3, it should print out 3, 10, 5, 16, 8, 4, 2, 1
 
+number=3
+while number != 1:
+    if number % 2 == 0: #even
+        number=(number/2)
+    elif number % 2 != 0: #odd
+        number=(number*3)+1
+    print(number)
+print("done!")
 
+    #10 5 
 
 # How would we print out each element in a list?
-fruits = ["apple", "banana", "cherry"]
+fruits = ["apple", "banana", "cherry", "dragonfruit", "eucalyptus", "frangipane"]
 # Gaurav to implement with while loop, and then with a for loop
+index = 0
+while index<3: #bc only three in list
+    print(fruits[index])
+    index = index+1
+print("done!")
+
+#if we don't want to hardcode the value of index
+index = 0
+while index<len(fruits):
+    #length of an array = len function
+    print(fruits[index])
+    index = index +1
+
+#cleaner version
+for fruit in fruits:
+    print(fruit)
 
 
 # Range is a function that returns a list of numbers from 0 to n-1
-for number in range(5):
+for number in range(1, 5):
     print(number)
 
 
@@ -62,13 +87,27 @@ movies = [
 ]
 
 # Task: Print out the name of each movie in the list
+# for element in array
+#   do something with element
 
+for movie_info in movies:
+    print(movie_info["title"])
+#want the variable to describe what it's holding to make the code easier to read
 
-# Nested loops!
+# Nested for loops!
 
 # Building a times table
 for i in range(1,6):
     for j in range(1,6):
-        print(i + "x" + j + "=" + i * j)
+        print(str(i) + "x" + str(j) + "=" + str(i * j))
 
-# How would we print out the cast of each movie in the list?
+
+for movie in movies:
+    cast_array = movie["cast"]
+    for cast_member in cast_array:
+        print(cast_member["name"])
+    
+    # How would we print out the cast member name of each movie in the list?
+for movie in movies: #movie is a type dictionary, movies is a type array
+    for name in movie:
+        print(cast)
